f9f84a3bfe57dbc1abbe8514368846c76ce4c64a
Alvin Muniz <muniz.alvin@gmail.com>
2021-05-04 16:23:24 -0500

    added home, cities and landing page component. Will refactor earlier piecies into the landing page component to allow for dynamic content creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './header/header.component';
 import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HomeComponent } from './home/home.component';
+import { CitiesComponent } from './cities/cities.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    JumbotronComponent
+    JumbotronComponent,
+    LandingPageComponent,
+    HomeComponent,
+    CitiesComponent,
   ],
   imports: [
     BrowserModule,
