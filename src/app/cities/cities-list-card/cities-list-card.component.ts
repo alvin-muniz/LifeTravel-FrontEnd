67e4352fd048103eae560a121b60ee1c503ace1c
Alvin Muniz <muniz.alvin@gmail.com>
2021-05-05 17:05:39 -0500

    pushed city information down to the card file

--- src/app/cities/cities-list-card/cities-list-card.component.ts
+++ src/app/cities/cities-list-card/cities-list-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-cities-list-card',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cities-list-card.component.less']
 })
 export class CitiesListCardComponent implements OnInit {
+
+  @Input() city: any;
 
   constructor() { }
 
