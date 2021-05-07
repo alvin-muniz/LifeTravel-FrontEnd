67aa0240e21ab4453b8e310a2860b205bfdc3ad0
Alvin Muniz <muniz.alvin@gmail.com>
2021-05-07 10:15:09 -0500

    updated styling on cities page to my liking

--- src/app/shared/post.service.ts
+++ src/app/shared/post.service.ts
@@ -8,23 +8,24 @@ export class PostService {
   cities: any[] = [
     {
       id: 1,
-      name: 'Murphysboro',
+      name: 'Opelika',
       state: 'Alabama',
-      landmark: 'Museum of Bama',
-      zip: '38299',
+      landmark: 'The old general store',
+      zip: '36804',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Railroad_Avenue_Historic_District_Opelika_Alabama.JPG/1024px-Railroad_Avenue_Historic_District_Opelika_Alabama.JPG',
       posts: [{
         id: 1,
         username: 'John',
         content: 'I had a great trip',
         date: '01-22-20',
-        photo: 'imageURL'
+        photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Snowboarding.jpg/1280px-Snowboarding.jpg'
       },
         {
           id: 2,
           username: 'Joeseph',
           content: 'I had a great trip',
           date: '01-22-20',
-          photo: 'imageURL'
+          photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Hiking_to_the_Ice_Lakes._San_Juan_National_Forest%2C_Colorado.jpg/1024px-Hiking_to_the_Ice_Lakes._San_Juan_National_Forest%2C_Colorado.jpg'
         }]
     },
     {
@@ -33,19 +34,20 @@ export class PostService {
       state: 'Georgia',
       landmark: 'Museum of Bama',
       zip: '38299',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Railroad_Avenue_Historic_District_Opelika_Alabama.JPG/1024px-Railroad_Avenue_Historic_District_Opelika_Alabama.JPG',
       posts: [{
         id: 1,
         username: 'Betty',
         content: 'I had a great trip',
         date: '01-22-20',
-        photo: 'imageURL'
+        photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Snowboarding.jpg/1280px-Snowboarding.jpg'
       },
         {
           id: 2,
           username: 'Jonathan',
           content: 'I had a great trip',
           date: '01-22-20',
-          photo: 'imageURL'
+          photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Hiking_to_the_Ice_Lakes._San_Juan_National_Forest%2C_Colorado.jpg/1024px-Hiking_to_the_Ice_Lakes._San_Juan_National_Forest%2C_Colorado.jpg'
         }]
     }
   ];
